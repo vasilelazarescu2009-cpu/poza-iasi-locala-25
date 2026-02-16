00151777500f91ab38f7bc4ac5019f459e62a159
vasilelazarescu2009-cpu <vasilelazarescu2009@gmail.com>
2026-02-16 13:50:10 +0200

    ver5

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 ofstream out("poza.out");
-
+ifstream in("poza.in");
+//adaugari erhan
 void sortare(int n, int x[])
 {
     int i,j;
@@ -21,79 +22,39 @@ void afisare(int n, int x[])
     for(i=0;i<n;i++)
         out<<x[i]<<" ";
 }
+//adaugari autor
 int main()
 {
     int n=6,x[]={1,2,100,3,4,5};
     sortare(n,x);
     afisare(n,x);
-    /*
+
     ifstream in("poza.in");
     ofstream out("poza.out");
 
     int C, n;
     in >> C >> n;
-    int A[3][n];
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < n; j++){
-            in >> A[i][j];
-        }
-    }
-    if(C == 1){
+    vector<int> A(n*3);
+    int a = n * 3;
+    for(int &i : A)
+        in >> i;
+    if(C==1){
         int Min = INT_MAX, Max = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < n; j++){
-                if(A[i][j] > Max)
-                    Max = A[i][j];
-                if(A[i][j] < Min)
-                    Min = A[i][j];
-            }
+        for(int i : A){
+            if(i < Min)
+                Min = i;
+            if(i > Max)
+                Max = i;
         }
-        out << Max << " " << Min;
+        out << Min << " " << Max;
         return 0;
-
     }
-    if(C == 2){
-        int B[3][n];
-        int Min = INT_MAX, prevMin = -1;
-        int Count = 0;
-        int row = 0;
-        bool order;
-        do{
-        order = true;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < n; j++){
-
-                if(prevMin < A[i][j] && A[i][j] < Min){
-                    Min = A[i][j];
-                    B[row][Count]=Min;
-                    Count++;
-                    if(Count >= n){
-                        Count = 0;
-                        row++;
-                    }
-                    prevMin = Min;
-                    Min = INT_MAX;
-                }
-
-                }
-        }
-            for(int k = 0; k < 3; k++){
-                    for(int l = 0; l < n; l++){
-                        if(B[k][l] > B[k+1][l+1])
-                            order = false;
-                    }
-                }
-
-        }while(order == false);
-
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < n; j++){
-                out << B[i][j] << " ";
-            }
-            out << endl;
-        }
+    if(C==2){
+        sortare()
     }
 
-*/
+
+
+
     return 0;
 }
